4fd8e56cd9cadc56af999d406c52bb8aec56841c
JiAe <kjm1709@naver.com>
2024-03-25 21:23:47 +0900

    get_data.py -> original.py 이름 변경 및 데이터 구조 변경

--- weather/data/original.py
+++ weather/data/original.py
@@ -2,10 +2,13 @@ import root
 import json
 import glob
 import requests
+import numpy as np
 import xarray as xr
 import pandas as pd
 import geopandas as gpd
 from shapely import wkt
+from sklearn.neighbors import KNeighborsClassifier
+from concurrent.futures import ProcessPoolExecutor
 
 
 with open(root.get_path('config'), 'r') as config_file:
@@ -24,65 +27,94 @@ class weather:
         self.csv_files_pattern = root.get_path('csv_weather_pattern')
 
         # 국가 코드, 경계면 포함
-        self.state = pd.read_csv(root.get_path('cleaned_state'))
+        self.state = pd.read_csv(root.get_path('state'))
         self.state['GEOMETRY'] = self.state['GEOMETRY'].apply(lambda x: wkt.loads(x)) # WKT 문자열을 공간 데이터 객체로 변환
         self.state = gpd.GeoDataFrame(self.state, geometry='GEOMETRY')
 
     ''' ----------------------------------------- 날씨 전체 데이터 return ----------------------------------------- '''
 
     def get(self):
-        csv_df = self.csv_data()
-        h5_df = self.h5_data()
+        csv_df = self.csv_data().groupby(['DATE', 'TIME_GROUP', 'CITY'], as_index=False).mean().reset_index(drop=True)
+        h5_df = self.h5_data().groupby(['DATE', 'TIME_GROUP', 'CITY'], as_index=False).mean().reset_index(drop=True)
 
-        csv_df.drop(columns=['geometry'], inplace=True)
-        csv_df = csv_df.groupby(['COUNTRY_CODE', 'DISTRICT', 'TIME'], as_index=False).mean().reset_index(drop=True)
-        csv_df.sort_values(by=['TIME', 'COUNTRY_CODE', 'DISTRICT'], inplace=True)
+        merged = pd.merge(csv_df, h5_df, on=['DATE', 'TIME_GROUP', 'CITY'], how='inner', validate="one_to_one")
 
-        h5_df.drop(columns=['geometry'], inplace=True)
-        h5_df = h5_df.groupby(['COUNTRY_CODE', 'DISTRICT', 'TIME'], as_index=False).mean().reset_index(drop=True)
-        h5_df.sort_values(by=['TIME', 'COUNTRY_CODE', 'DISTRICT'], inplace=True)
+        cols = ['DATE', 'TIME_GROUP', 'CITY', 'AIR_PRESSURE', 'AIR_TEMPERATURE', 'HUMIDITY', 'RAINFALL', 'SNOWFALL']
+        merged = merged[cols]
 
-        merged = pd.merge(h5_df, csv_df, on=['TIME', 'COUNTRY_CODE', 'DISTRICT'], how='outer', validate="many_to_many")
+        return merged[merged['DATE'].apply(lambda x: x.year) == 2019].dropna().sort_values(by=['DATE', 'TIME_GROUP', 'CITY'])
 
-        cols = ['COUNTRY_CODE', 'DISTRICT', 'TIME', 'AIR_PRESSURE', 'AIR_TEMPERATURE', 'HUMIDITY', 'WIND_FROM_DIRECTION', 'WIND_SPEED', 'RAINFALL', 'SNOWFALL']
-        dataframe = merged[cols]
-        return dataframe
+    ''' **----------------------------------------- 시간별로 합치기 -----------------------------------------** '''
+    def time_group(self, df):
+        bins = [-1, 6, 12, 18, 24]
+        labels = ['0-6', '6-12', '12-18', '18-24']
+        df['DATE'] = df['TIME'].dt.date
+        df['TIME_GROUP'] = pd.cut(df['TIME'].dt.hour, bins=bins, labels=labels, right=True)
+        df.drop(columns=['TIME'], inplace=True)
+        df = df.groupby(['DATE', 'TIME_GROUP', 'LAT', 'LNG'], as_index=False).mean().reset_index(drop=True)
+        return df.dropna()
+
+    ''' **----------------------------------------- 가까운 도시 찾기 -----------------------------------------** '''
+    def near_city(self, df):
+        """
+        weather: 날씨 데이터 dataframe
+        return: 날씨 데이터 dataframe 필터링 + 도시
+        """
+        df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['LNG'], df['LAT']))
+        df = gpd.sjoin(df, self.state, how="left", predicate="within")
+
+        city = pd.read_csv(root.get_path('city'))
+        city['POINT'] = city['POINT'].apply(lambda x: wkt.loads(x))
+
+        temp = []
+
+        for district, group in df.groupby('DISTRICT'):
+            selected = city[city['DISTRICT'] == district]
+            if selected.empty or district is None:
+                continue
+            X = np.array([[point.x, point.y] for point in selected['POINT']])
+            y = selected['CITY'].values
+
+            knn = KNeighborsClassifier(n_neighbors=1)
+            knn.fit(X, y)
+            new_point = np.array([[point.x, point.y] for point in group['geometry']])
+            group['CITY'] = knn.predict(new_point)
+            group.drop(columns=['geometry', 'DISTRICT', 'COUNTRY_CODE'], inplace=True)
+            temp.append(group)
+        df = pd.concat(temp).groupby(['DATE', 'TIME_GROUP', 'CITY'], as_index=False).mean().reset_index(drop=True)
+        return df.dropna()
 
     ''' ----------------------------------------- 기압, 온도, 습도, 풍속, 바람 방향(북쪽 기준 각도) ----------------------------------------- '''
 
     def csv_data(self):
-        use_cols = ['actual_time', 'air_pressure', 'air_temperature', 'relative_humidity',
-                    'wind_from_direction', 'wind_speed', 'latitude', 'longitude']
+        use_cols = ['actual_time', 'air_pressure', 'air_temperature', 'relative_humidity', 'latitude', 'longitude']
         change_columns = {
-            'actual_time': 'TIME', 'latitude': 'lat', 'longitude': 'lng', 'air_pressure': 'AIR_PRESSURE',
-            'air_temperature': 'AIR_TEMPERATURE', 'relative_humidity': 'HUMIDITY',
-            'wind_from_direction': 'WIND_FROM_DIRECTION', 'wind_speed': 'WIND_SPEED'
+            'actual_time': 'TIME', 'latitude': 'LAT', 'longitude': 'LNG',
+            'air_pressure': 'AIR_PRESSURE', 'air_temperature': 'AIR_TEMPERATURE',
+            'relative_humidity': 'HUMIDITY'
         }
 
         files = glob.glob(self.csv_files_pattern)
         concat_data = pd.DataFrame()
         for file in files:
-            data = pd.read_csv(file, comment='#', usecols=use_cols).drop_duplicates(
-                subset=['actual_time', 'latitude', 'longitude'], keep='first'
-            ).rename(columns=change_columns)
+            data = pd.read_csv(file, comment='#', usecols=use_cols).rename(columns=change_columns)
+            data = data.groupby(['TIME', 'LAT', 'LNG'], as_index=False).mean().reset_index(drop=True)
             data['TIME'] = pd.to_datetime(data['TIME'])
+            data = data.astype({'LAT': 'float32', 'LNG': 'float32'})
+            data = self.time_group(data)
 
             if not concat_data.empty:
                 concat_data = pd.concat([concat_data, data])
             else:
                 concat_data = data
 
-        concat_data['TIME'] = concat_data['TIME'].dt.round('H') # 가까운 시간 반올림 0분 0초
-
-        concat_data = gpd.GeoDataFrame(concat_data, geometry=gpd.points_from_xy(concat_data['lng'], concat_data['lat']))
-        concat_data = gpd.sjoin(concat_data, self.state, how="inner", predicate="within")
-        concat_data = concat_data.groupby(['TIME', 'COUNTRY_CODE', 'DISTRICT', 'geometry'], as_index=False).median().reset_index(drop=True)
-        return concat_data[['TIME', 'COUNTRY_CODE', 'DISTRICT', 'geometry', 'AIR_PRESSURE', 'AIR_TEMPERATURE', 'HUMIDITY', 'WIND_FROM_DIRECTION', 'WIND_SPEED']]
+        concat_data = self.near_city(concat_data)
+        return concat_data[['DATE', 'TIME_GROUP', 'CITY', 'AIR_PRESSURE', 'AIR_TEMPERATURE', 'HUMIDITY']].dropna()
 
     ''' ----------------------------------------- 강수량, 강설량 ----------------------------------------- '''
 
     def h5_data(self):
-        dataframe_list = []
+        dataframes = []
 
         for col in self.h5_cols:
             dataframe = pd.DataFrame()
@@ -90,51 +122,44 @@ class weather:
 
             for file in files:
                 print(file)
-                df = self.__partial_h5_data(path=file, col=col)
+                df = self.__partial_h5_data(path=file, col=col).rename(columns={'time': 'TIME', 'lat': 'LAT', 'lng': 'LNG'})
+                df = self.time_group(df)
+                df = self.near_city(df)
                 dataframe = pd.concat([dataframe, df], ignore_index=True)
-            dataframe_list.append(dataframe)
+            dataframes.append(dataframe)
 
         merged_df = None
 
-        for df in dataframe_list:
-            df = df.groupby(['time', 'lat', 'lng'], as_index=False).mean().reset_index(drop=True)
+        for df in dataframes:
+            print(df.columns)
+            df = df.groupby(['DATE', 'TIME_GROUP', 'CITY'], as_index=False).mean().reset_index(drop=True)
             if merged_df is None:
                 merged_df = df
             else:
-                merged_df = pd.merge(merged_df, df, on=['time', 'lat', 'lng'], how='inner', validate="one_to_one")
+                merged_df = pd.merge(merged_df, df, on=['DATE', 'TIME_GROUP', 'CITY'], how='inner', validate="one_to_one").dropna()
 
-        merged_df['time'] = merged_df['time'].dt.round('H')  # 가까운 시간 반올림 0분 0초
+        merged_df.rename(columns={'Rainf': 'RAINFALL', 'Snowf': 'SNOWFALL'}, inplace=True)
 
-        merged_df.rename(columns={'time': 'TIME', 'Rainf': 'RAINFALL', 'Snowf': 'SNOWFALL'}, inplace=True)
-        merged_df = gpd.GeoDataFrame(merged_df, geometry=gpd.points_from_xy(merged_df['lng'], merged_df['lat']))
-        merged_df = gpd.sjoin(merged_df, self.state, how="inner", predicate="within")
-        merged_df = merged_df.groupby(['TIME', 'COUNTRY_CODE', 'DISTRICT', 'geometry'], as_index=False).median().reset_index(drop=True)
-
-        return merged_df.reset_index(drop=True)[['TIME', 'COUNTRY_CODE', 'DISTRICT', 'geometry', 'RAINFALL', 'SNOWFALL']]
+        return merged_df[['DATE', 'TIME_GROUP', 'CITY', 'RAINFALL', 'SNOWFALL']]
 
     ''' **----------------------------------------- 파일 별 (강수량, 강설량) -----------------------------------------** '''
 
     def __partial_h5_data(self, **kwargs):
         """
         path: h5 파일 경로
-        cities_gcs: (위도, 경도)
         col: 선택 파일 컬럼
         :return: dataframe
         """
         path = kwargs.get('path')
         col = kwargs.get('col')
 
-        lat_min, lat_max = 34, 71
-        lng_min, lng_max = -10, 30
-
         ds = xr.open_dataset(path, engine='netcdf4')
         select = ds[['time', 'lat', 'lon', col]]
-        df = select.where((select['lat'] >= lat_min) & (select['lat'] <= lat_max) & (select['lon'] >= lng_min) & (select['lon'] <= lng_max), drop=True)
-        df = df.to_dataframe().reset_index(drop=True).rename(columns={'lon': 'lng'})
-
+        df = select.to_dataframe().reset_index(drop=True).rename(columns={'lon': 'lng'})
+        df = df.astype({'lat': 'float32', 'lng': 'float32'})
         df['time'] = pd.to_datetime(df['time'], unit='s').dt.tz_localize('UTC')
 
-        return df
+        return df.groupby(['time', 'lat', 'lng'], as_index=False).mean().reset_index(drop=True)
 
 ''' =========================================== 음악 취향 =========================================== '''
 
@@ -146,7 +171,7 @@ class last_fm:  # Last.fm API
 
     def get_data(self):
         data = {}
-        euros = pd.read_csv(root.get_path('cleaned_country'))['COUNTRY_NAME']
+        euros = pd.read_csv(root.get_path('country'))['COUNTRY_NAME']
         euros = pd.concat([euros, pd.Series(['Korea, Republic of'])], ignore_index=True)
         for name in euros:
             data[name] = self.__get_country_tags(name)
